1285c0e02de16acaa7ab6ee84bb539262aeccc28
kikuchi wataru <kikuchi@kikuchi.wataru.com>
2016-05-12 18:12:08 +0900

    [add].gitconfig

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
