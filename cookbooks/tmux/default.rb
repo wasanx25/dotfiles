9221b7d9ff3baf99839b82ee793d2e3091b3de7b
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-10-16 01:40:55 +0900

    Add tmux recipe

---
+++ cookbooks/tmux/default.rb
@@ -0,0 +1,4 @@
+link "#{ENV['HOME']}/.tmux.conf" do
+  to File.expand_path('.config/tmux/.tmux.conf')
+  force true
+end
--- roles/darwin/default.rb
+++ roles/darwin/default.rb
@@ -2,3 +2,4 @@ include_recipe File.expand_path('../../../lib/recipe_helper.rb', __FILE__)
 
 include_cookbook 'brew'
 include_cookbook 'zsh'
+include_cookbook 'tmux'
