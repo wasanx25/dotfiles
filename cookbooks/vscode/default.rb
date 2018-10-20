b4671042d369468503bcee0221f271548d991b2a
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-10-20 17:27:17 +0900

    Add symlink for vscode cli

--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -1,3 +1,7 @@
+link '/usr/local/bin' do
+  to '/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code'
+end
+
 execute 'cat extensions.txt | xargs -L 1 code --install-extension' do
   cwd File.expand_path('../files', __FILE__)
 end
