1044b7dff3cb90231c21b7b17aee2dc25a86d644
wasanx25 <wasanx25@gmail.com>
2018-11-16 00:45:20 +0900

    Fix exactly pkg build

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -5,12 +5,17 @@ when 'arch'
       repository 'https://aur.archlinux.org/ghq.git'
     end
 
-    execute 'makepkg -S' do
+    execute 'makepkg' do
       cwd "#{ENV['HOME']}/ghq"
     end
 
     execute 'pacman -U ghq-*.pkg.tar.xz' do
+      user 'root'
       cwd "#{ENV['HOME']}/ghq"
+    end
+
+    execute "rm -rf #{ENV['HOME']}/ghq" do
+      only_if "test -d #{ENV['HOME']}/ghq"
     end
   end
 end
