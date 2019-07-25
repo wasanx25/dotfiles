5e410672268001ab34746575d5bf0cbbc4c4c860
wasanx25 <wasanx25@gmail.com>
2019-07-25 09:54:40 +0900

    Follow XDG base directory specification

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -1,9 +1,11 @@
-link "#{ENV['HOME']}/.gitconfig" do
+directory "#{ENV['HOME']}/.config/git"
+
+link "#{ENV['HOME']}/.config/git/config" do
   to File.expand_path('.config/git/.gitconfig')
   force true
 end
 
-link "#{ENV['HOME']}/.gitignore_global" do
+link "#{ENV['HOME']}/.config/git/ignore" do
   to File.expand_path('.config/git/.gitignore_global')
   force true
 end
