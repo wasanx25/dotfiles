fc053f7d74ac035aa381294981b4cdabe220bc55
wasanx25 <wasanx25@gmail.com>
2024-03-20 16:40:16 +0900

    Comment out install dein.vim

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -3,9 +3,12 @@ link "#{ENV['HOME']}/.vimrc" do
   force true
 end
 
-execute 'curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh'
-execute 'sh ./installer.sh ~/.cache/dein'
-execute 'rm ./installer.sh'
+# TODO: must skip prompt
+# execute 'curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh'
+# execute 'sh ./installer.sh ~/.cache/dein'
+# execute 'rm ./installer.sh'
+
+directory "#{ENV['HOME']}/.vim"
 
 link "#{ENV['HOME']}/.vim/dein.toml" do
   to File.expand_path('.config/editors/vim/dein.toml')
