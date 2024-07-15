9f774b1dbaf3a8c6178510195e7ec0978d97f18e
wasanx25 <wasanx25@gmail.com>
2024-07-15 15:34:12 +0900

    Remove

--- cookbooks/gitkraken/default.rb
+++
@@ -1,8 +0,0 @@
-case node[:platform]
-when 'arch'
-  package 'libcurl-gnutls'
-  package 'libgnome-keyring'
-  pacman_build 'gitkraken' do
-    app_path '/opt/gitkraken'
-  end
-end
--- cookbooks/goland/default.rb
+++
@@ -1,8 +0,0 @@
-case node[:platform]
-when 'arch'
-  package 'rsync'
-  package 'jdk-openjdk'
-  pacman_build 'goland-eap' do
-    app_path '/opt/goland-eap'
-  end
-end
